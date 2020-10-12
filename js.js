87278d6eb975e939341d9ebb12005cb3fef361c2
Alex <alexkateudall@gmail.com>
2020-10-13 12:59:21 +1300

    finished

--- js.js
+++ js.js
@@ -42,11 +42,36 @@ document.getElementById('malebutton').addEventListener('click', () => {
   for (let i=0; i < totalellipses.length; i++) {
     gsap.to(totalellipses[i], 0, { scale:0 })
   }
-  
-
-  // show all total ellipses
+     
+  // hide all females
+  for (let i=0; i < femaleellipses.length; i++) {
+    gsap.to(femaleellipses[i], 0, { scale:0 })
+  }  
+    
+  // show all male ellipses
   for (let i=0; i < maleellipses.length; i++) {
     gsap.to(maleellipses[i], 1, { 
+      scale: 1, 
+      transformOrigin: 'center center',
+      ease: "elastic.out(1, 0.3)"
+    });
+  }
+})
+
+document.getElementById('femalebutton').addEventListener('click', () => {
+  // hide all total
+  for (let i=0; i < totalellipses.length; i++) {
+    gsap.to(totalellipses[i], 0, { scale:0 })
+  }
+    
+  // hide all males
+  for (let i=0; i < maleellipses.length; i++) {
+    gsap.to(maleellipses[i], 0, { scale:0 })
+  } 
+    
+  // show all female ellipses
+  for (let i=0; i < femaleellipses.length; i++) {
+    gsap.to(femaleellipses[i], 1, { 
       scale: 1, 
       transformOrigin: 'center center',
       ease: "elastic.out(1, 0.3)"
