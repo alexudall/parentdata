7fe65ec5bbaffa48505abc9e431ce4e7986b2676
Alex <alexkateudall@gmail.com>
2020-10-13 12:12:17 +1300

    in class

--- js.js
+++ js.js
@@ -1,23 +1,55 @@
 /*eslint-env browser*/
 
+// grab all ellipses
+let totalellipses = document.querySelectorAll('#total ellipse');
+let maleellipses = document.querySelectorAll('#male ellipse');
+let femaleellipses = document.querySelectorAll('#female ellipse');
 
+// hid all of the ellipses
+
+for (let i=0; i < totalellipses.length; i++) {
+    gsap.to(totalellipses[i], 0, { scale:0 })
+}
+for (let i=0; i < maleellipses.length; i++) {
+    gsap.to(maleellipses[i], 0, { scale:0 })
+}
+for (let i=0; i < femaleellipses.length; i++) {
+    gsap.to(femaleellipses[i], 0, { scale:0 })
+}
+
+// code for total button 
 document.getElementById('totalbutton').addEventListener('click', () => {
-  document.getElementById('total').style.opacity = 1;
-  document.getElementById('female').style.opacity = 0;
-  document.getElementById('male').style.opacity = 0;
-  
-});
+  // hide all males
+  for (let i=0; i < maleellipses.length; i++) {
+    gsap.to(maleellipses[i], 0, { scale:0 })
+  }
+  // hide all females
+  for (let i=0; i < femaleellipses.length; i++) {
+    gsap.to(femaleellipses[i], 0, { scale:0 })
+  }
+  // show all total ellipses
+  for (let i=0; i < totalellipses.length; i++) {
+    gsap.to(totalellipses[i], 1, { 
+      scale: 1, 
+      transformOrigin: 'center center',
+      ease: "elastic.out(1, 0.3)"
+    });
+  }
+})
 
 document.getElementById('malebutton').addEventListener('click', () => {
-  document.getElementById('male').style.opacity = 1;
-  document.getElementById('female').style.opacity = 0;
-  document.getElementById('total').style.opacity = 0;
-});
+  // hide all total
+  for (let i=0; i < totalellipses.length; i++) {
+    gsap.to(totalellipses[i], 0, { scale:0 })
+  }
+  
 
-document.getElementById('femalebutton').addEventListener('click', () => {
-  document.getElementById('female').style.opacity = 1;
-  document.getElementById('male').style.opacity = 0;
-  document.getElementById('total').style.opacity = 0;
-});
-
-
+  // show all total ellipses
+  for (let i=0; i < maleellipses.length; i++) {
+    gsap.to(maleellipses[i], 1, { 
+      scale: 1, 
+      transformOrigin: 'center center',
+      ease: "elastic.out(1, 0.3)"
+    });
+  }
+})
